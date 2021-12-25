e288a6166959250ab6d7ac8bcef695f12b821cac
TTiip <2711430287@qq.com>
2021-12-25 17:44:36 +0800

    6_Request入门01

--- 第一章/6_Request入门01.py
+++ 第一章/6_Request入门01.py
@@ -9,12 +9,12 @@ import requests
 query = input('输入一个你喜欢的明星：')
 url = f'https://www.sogou.com/web?query={query}'
 # 请求头 标识请求设备（绕过反爬）
-headers_obj = {
+headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'
 }
 
 # 地址栏里面的请求 一定是get请求
-resp = requests.get(url, headers=headers_obj)
+resp = requests.get(url, headers=headers)
 
 # print(resp) # <Response [200]>
 print(resp.text) # 拿到页面源代码
